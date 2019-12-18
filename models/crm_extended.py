23788b3cbea6fcbffa7845892635863720e5fdb7
Alejandro Sanchez <trabajosoftnet@outlook.es>
2019-12-18 15:03:34 -0500

    En esta version del desarrollo busco hacer los presupuestos derivados con un campo many2one

--- models/crm_extended.py
+++ models/crm_extended.py
@@ -11,6 +11,7 @@ class Lead(models.Model):
 
     @api.model
     def create(self, vals):
+        # This code block is added by Odoolibre
         seq = self.env['ir.sequence'].next_by_code('crm.lead') or _('New')
         vals['name'] = seq
 
@@ -30,3 +31,19 @@ class Lead(models.Model):
         onchange_values.update(vals) # we don't want to overwirte any existing key
         vals = onchange_values
         return super(Lead, self.with_context(context,mail_create_nolog=True)).create(vals)
+
+
+
+
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+
+    parent = fields.Many2one('sale.order.line', string='Parent')
+    child = fields.Boolean(string='Child')
+
+
+
+
