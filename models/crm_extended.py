a432ad657ada351e80d6c94c965225155269e390
Alejandro Sanchez <alejandroingelec19@gmail.com>
2020-01-09 10:24:21 -0400

    se modifica metodo create

--- models/crm_extended.py
+++ models/crm_extended.py
@@ -24,31 +24,16 @@ class Lead(models.Model):
 
 
 
-
-
     @api.model
     def create(self, vals):
         # This code block is added by Odoolibre
-        seq = self.env['ir.sequence'].next_by_code('crm.lead') or _('New')
+        seq = self.env['ir.sequence'].next_by_code('crm.lead')
         vals['name'] = seq
 
-        # set up used to find the lead's Sale Team which is needed
-        # to correctly set the default stage_id
-        context = dict(self._context or {})
-        if vals.get('type') and not self._context.get('default_type'):
-            context['default_type'] = vals.get('type')
-        if vals.get('team_id') and not self._context.get('default_team_id'):
-            context['default_team_id'] = vals.get('team_id')
-
-        if vals.get('user_id') and 'date_open' not in vals:
-            vals['date_open'] = fields.Datetime.now()
-
-        partner_id = vals.get('partner_id') or context.get('default_partner_id')
-        onchange_values = self._onchange_partner_id_values(partner_id)
-        onchange_values.update(vals) # we don't want to overwirte any existing key
-        vals = onchange_values
-        return super(Lead, self.with_context(context,mail_create_nolog=True)).create(vals)
-
+        result = super().create(vals)
+        _logger.info("\n\n create modificado es: %s\n\n", result)
+        
+        return result
 
 
 class SaleOrderNew(models.Model):
