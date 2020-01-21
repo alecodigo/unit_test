0ba6c78c00b2d7b06d35ac5e702e3084be1c9af9
Alejandro Sanchez <alejandroingelec19@gmail.com>
2020-01-20 23:21:30 -0400

    se eliminan los _logger.info y se descomenta el fragmento de codigo que cancela los registros child al aprobar uno de ellos

--- models/crm_extended.py
+++ models/crm_extended.py
@@ -5,7 +5,6 @@ import logging
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
 
-_logger = logging.getLogger(__name__)
 
 
 class Lead(models.Model):
@@ -66,29 +65,20 @@ class SaleOrderNew(models.Model):
         
         data = {}
         
-    
-        
-        # stage 1 cancel all child records
-        #records_id = self.env['sale.order'].search([('parent_id', '=', self.parent_id.id),('child', '=', True)])
-        #if records_id:
-        #    for record in records_id:
-        #        record.write({'state': 'cancel'})
+        records_id = self.env['sale.order'].search([('parent_id', '=', self.parent_id.id),('child', '=', True)])
+        if records_id:
+            for record in records_id:
+                record.write({'state': 'cancel'})
         
         # stage 2 search the parent record
         parent = self.env['sale.order'].search([('id', '=', self.parent_id.id),('child', '=', False)]) 
-        _logger.info("\n\n partner_id.order_line %s\n\n", parent.order_line)
-        
-        # stage 3 si es verdadero comienzo a verificar 
-        # si actualizo o creo un registro
-        # verifico si existe en verdad un registro padre
+
         if parent:
             if self.order_line:
                 res = []
                 for item in self.order_line:
-                    _logger.info("\n\n item.id_inherit %s\n\n",item.id_inherit)
                     # update
                     parent_id = self.env['sale.order'].browse(self.parent_id.id).order_line.ids
-                    _logger.info("\n\n item.id_inherit %s parent_id %s\n\n", item.id_inherit, parent_id)
                     if item.id_inherit in parent_id:
                         res.append((1,item.id_inherit, {
                             'product_id': item.product_id.id,
@@ -126,7 +116,6 @@ class SaleOrderNew(models.Model):
 
                        })
 
-        _logger.info("Que mas contiene data %s", data)
         parent.write(data)
 
         self.confirmed = True
@@ -195,7 +184,6 @@ class SaleOrderNew(models.Model):
 
         # El campo child No esta establecido
         if vals['child'] == False:
-            #if result.name == _('New'):
             if 'company_id' in vals:
                 vals['name'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code('sale.order')
             else:
@@ -203,10 +191,7 @@ class SaleOrderNew(models.Model):
             result = super().create(vals)
             return result
         
-        #El campo child esta establecido
-        else:
-            
-            #parent = self.search([('parent_id', '=', self.parent_id.id),('child', '=', False)], limit=1)
+        else:            
             idx = vals['parent_id']
             parent = self.browse(idx)
             account = self.search_count([('parent_id', '=', idx),('child', '=', True)])
